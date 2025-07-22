387215a3187f8b3dbea89498c3fa2e3bbdb9a7e3
Syed Ahtisham <ahtisham282000@gmail.com>
2025-07-22 13:43:49 +0500

    change routing  Mechanism

--- src/App.jsx
+++ src/App.jsx
@@ -1,68 +1,23 @@
-// import React from 'react'
-// import { RouterProvider } from 'react-router-dom'
-// import router from './AppRoutes/MainAppRoutes'
-
-// const App = () => {
-// return (
-// <RouterProvider router={router}/>
-// )
-// }
-// export default App
-
 import React from 'react';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter } from 'react-router-dom';
 
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
 
-
-
-import Navbar from './Components/Frontend/Navbar';
+import Navbar from './components/Frontend/Navbar';
 import HeroSection from './components/Frontend/HeroSection';
-import BrandSlider from './components/Frontend/BrandSlider';
-import ProductSection from './components/Frontend/ProductSection';
-import AlwaysOnTrend from './components/Frontend/AlwaysOnTrend';
-import WhyChooseUs from './components/Frontend/WhyChooseUs';
-import Footer from './components/Frontend/Footer';
-
-
-import Cart from './pages/Frontend/Viewcart';
-import ContactSection from './pages/Frontend/ContactSection'; // import your contact page component
-import AboutPage from './pages/Frontend/About'; // if you have an about page
-import ProductDetail from "./pages/Frontend/ProductDetail"; // import your product detail page
+import Frontend from '././AppRoutes/Frontend'; // Import Frontend
+import Footer from '././components/Frontend/Footer';
 
 export default function App() {
   return (
-    <>
-      <BrowserRouter>
-        <Navbar />
-        {/* <Navbar /> */}
-        <HeroSection />
-        <Routes>
-          <Route path="/" element={
-            <>
-              <BrandSlider />
-              <ProductSection title="Trending Now" />
-              <ProductSection title="Best Selling" />
-              <AlwaysOnTrend />
-              <WhyChooseUs />
-            </>
-          } />
-          <Route path="/contact" element={<ContactSection />} />
-          <Route path="/about" element={<AboutPage />} />
-          <Route path="/product/:productId" element={<ProductDetail />} />
-            <Route path="/cart" element={<Cart />} />
-          {/* Add other routes as needed */}
-        </Routes>
-        <Footer />
-        {/* <HeroSection />
-        <BrandSlider />
-        <ProductSection title="Trending Now" />
-        <ProductSection title="Best Selling" />
-        <AlwaysOnTrend />
-        <WhyChooseUs />
-        <Footer /> */}
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <Navbar />
+      <HeroSection />
+      <Frontend /> {/* Call the Frontend component here */}
+      <Footer />
+    </BrowserRouter>
+    
+    
   );
 }
--- src/AppRoutes/Frontend.jsx
+++ src/AppRoutes/Frontend.jsx
@@ -1,15 +1,31 @@
-import React from 'react'
-import FrontendLayout from '../Layout/Frontend/index' 
+import React from 'react';
+import { Routes, Route } from 'react-router-dom';
 
-const FrontendRoute = {
-    element:<FrontendLayout/>,
-    children:[
-        {
-            path:"/",
-            element:<h1>Home</h1>
-        }
-    ]
-        
-    }
+import BrandSlider from '../components/Frontend/BrandSlider';
+import ProductSection from '../components/Frontend/ProductSection';
+import AlwaysOnTrend from '../components/Frontend/AlwaysOnTrend';
+import WhyChooseUs from '../components/Frontend/WhyChooseUs';
+import ContactSection from '../pages/Frontend/ContactSection';
+import AboutPage from '../pages/Frontend/About';
+import ProductDetail from '../pages/Frontend/ProductDetail';
+import Cart from '../pages/Frontend/Viewcart';
 
-export default FrontendRoute
+export default function Frontend() {
+  return (
+    <Routes>
+      <Route path="/" element={
+        <>
+          <BrandSlider />
+          <ProductSection title="Trending Now" />
+          <ProductSection title="Best Selling" />
+          <AlwaysOnTrend />
+          <WhyChooseUs />
+        </>
+      } />
+      <Route path="/contact" element={<ContactSection />} />
+      <Route path="/about" element={<AboutPage />} />
+      <Route path="/product/:productId" element={<ProductDetail />} />
+      <Route path="/cart" element={<Cart />} />
+    </Routes>
+  );
+}
